afc76d9e613bd3dbd9bf5cb19476579427bdc757
Aman Singh <141247273+amanrss@users.noreply.github.com>
2026-06-09 15:49:42 +0530

    Update gemini_client.py

--- utils/gemini_client.py
+++ utils/gemini_client.py
@@ -1,17 +1,13 @@
 import google.generativeai as genai
 import streamlit as st
 
-try:
-    api_key = st.secrets["AQ.Ab8RN6LdrQmF6TTSyuMj-EkUm9MnKJ_ICCVgc1_Jee2NDhhfDA"]
-except Exception:
-    api_key = None
+api_key = st.secrets.get("AQ.Ab8RN6LdrQmF6TTSyuMj-EkUm9MnKJ_ICCVgc1_Jee2NDhhfDA")
 
 if api_key:
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel("gemini-2.5-flash")
 else:
     model = None
-
 
 def ask_gemini(prompt):
     if model is None:
