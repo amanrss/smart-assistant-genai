5597c56bffc2ebde15349a1040b63781bb953322
Aman Singh <141247273+amanrss@users.noreply.github.com>
2026-06-09 15:38:34 +0530

    Update challenge.py

--- utils/challenge.py
+++ utils/challenge.py
@@ -1,6 +1,6 @@
 from utils.gemini_client import ask_gemini
 
-def generate_questions(text):
+def generate_challenges(text):
 
     prompt = f"""
     Generate 5 challenging questions from the document.
@@ -9,6 +9,29 @@ def generate_questions(text):
 
     Document:
     {text}
+    """
+
+    return ask_gemini(prompt)
+
+
+def evaluate_answer(question, answer, document):
+
+    prompt = f"""
+    Document:
+    {document}
+
+    Question:
+    {question}
+
+    User Answer:
+    {answer}
+
+    Evaluate the answer.
+
+    Return:
+    Score /10
+    Correct Answer
+    Explanation
     """
 
     return ask_gemini(prompt)
